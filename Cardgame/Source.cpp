5c07329911193b87c8337bdb91c274480348c88b
MichaelVenturi <michaelventuri58@aol.com>
2022-03-18 02:54:15 -0400

    card enums setup

--- Cardgame/Source.cpp
+++ Cardgame/Source.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+#include "Card.h"
+
 using namespace std;
 
 int main() {
 	// github push
+
+	Suit s = Suit::hearts;
+	Rank r = Rank::jack;
+
+	//int x = 12 + static_cast<int>(r);
+
+	cout << s << " " << r << endl;
 
 	return 0;
 }
